a6477759c3b6f2b71151887b7260ea9bcbf6a0f6
Александр Чернышев <sanyachernishev@mail.ru>
2022-04-01 19:35:56 +0300

    init commit

---
+++ addressbook-web-test/addressbook-web-test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace addressbook_web_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
