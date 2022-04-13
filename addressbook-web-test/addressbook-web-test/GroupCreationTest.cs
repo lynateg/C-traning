da27f65293d64551c92a1f91577e370019cf1811
Александр Чернышев <sanyachernishev@mail.ru>
2022-04-14 00:52:58 +0300

    модификация для украшательства

--- addressbook-web-test/addressbook-web-test/GroupCreationTest.cs
+++ addressbook-web-test/addressbook-web-test/GroupCreationTest.cs
@@ -40,7 +40,7 @@ namespace WebAddressbookTests
         public void GroupCreationTest()
         {
             OpenHomePage();
-            Login("admin","secret");           
+            Login(new AccountData("admin","secret"));           
             GoToGroupsPage();            
             InitGroupCreation();   
             FillGroupForm("xxx","zzz","yyy");
@@ -82,13 +82,13 @@ namespace WebAddressbookTests
             driver.FindElement(By.LinkText("groups")).Click();
         }
 
-        private void Login(string username, string password)
+        private void Login(AccountData account)
         {
             driver.FindElement(By.Name("user")).Click();
             driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys(username);
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
             driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys(password);
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
             driver.FindElement(By.XPath("//input[@value='Login']")).Click();
         }
       
